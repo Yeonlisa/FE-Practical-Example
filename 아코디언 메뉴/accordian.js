09c2578ddbae8da36899797e65e70ed8c6d73ac5
yeonlisa <tkzw4869@gmail.com>
2022-03-30 11:04:15 +0900

    아코디언(accordion) 메뉴 만들기(5) - JavaScript 코드 작업 2

--- 아코디언 메뉴/accordian.js
+++ 아코디언 메뉴/accordian.js
@@ -1,12 +1,15 @@
 window.onload = () => {
+    // panel-faq-container
     const panelFaqContainer = document.querySelectorAll('.panel-faq-container');
-    console.log(panelFaqContainer); // NodeList 객체
+
+    // panel-faq-answer
+    let panelFaqAnswer = document.querySelectorAll('.panel-faq-answer');
 
     // 반복문 순회하면서 해당 FAQ 제목 클릭시 콜백 처리
     for(let i = 0; i < panelFaqContainer.length; i++) {
         panelFaqContainer[i].addEventListener('click', () => {
-            // 클릭시 처리 할 일
-            console.log('클릭'+ i);
+            // 클릭시 처리 할 일: FAQ 제목 클릭시 본문이 보이게끔 한다 -> active 클래스 추가
+            panelFaqAnswer[i].classList.add('active');
         });
     }
 }
