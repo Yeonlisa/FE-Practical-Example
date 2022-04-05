2e7d1aaa294c1229e125c27a548c085f9f5f6c7b
yeonlisa <tkzw4869@gmail.com>
2022-04-06 06:30:20 +0900

    로컬 데이터 - localStorage에 값 저장 및 가져오기(1)

---
+++ 로컬 데이터/localStorage.js
@@ -0,0 +1,6 @@
+// 브라우저 로컬 데이터에 저장하기
+
+// 주요 메서드 2개
+// 값 저장하기 -> localStorage.setItem('key',value);
+// 값 가져오기 -> localStorage.getItem('key');
+// 키(key)를 그대로 놓고 새로운 값을 저장하면 덮어쓰기가 되면서 기존 로컬 데이터 정보가 수정.
