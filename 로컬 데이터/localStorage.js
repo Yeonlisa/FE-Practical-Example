dc7d184a717382e8001ed2c42bcf803cba2908fc
yeonlisa <tkzw4869@gmail.com>
2022-04-06 06:48:42 +0900

    로컬 데이터 - localStorage에 값 저장 및 가져오기(3) - 스크립트 작성

--- 로컬 데이터/localStorage.js
+++ 로컬 데이터/localStorage.js
@@ -4,3 +4,43 @@
 // 값 저장하기 -> localStorage.setItem('key',value);
 // 값 가져오기 -> localStorage.getItem('key');
 // 키(key)를 그대로 놓고 새로운 값을 저장하면 덮어쓰기가 되면서 기존 로컬 데이터 정보가 수정.
+
+window.onload = () => {
+	
+	// Set Data 버튼
+	const btnSLD = document.querySelector( '.btnSetLocalData' );
+	console.log( btnSLD );
+	
+	// Input 텍스트 값
+	const input = document.querySelector( 'input' );
+	
+	// Set Data 버튼 클릭 시
+	btnSLD.addEventListener( 'click', () => {
+		
+		// 할 일 처리 --> 입력한 텍스트 값 가져오기
+		const inputValue = input.value;
+		console.log( inputValue );
+		
+		// localStorage에 저장하기
+		localStorage.setItem( 'userid', inputValue );
+		input.value = "";
+		
+	} );
+	
+	// Get Data 버튼 클릭 시
+	const btnGLD = document.querySelector( '.btnGetLocalData' );
+	console.log( btnGLD );
+	
+	btnGLD.addEventListener( 'click', () => {
+		
+		// 할 일 처리 --> 해당 키의 로컬 데이터가 있으면 가져오기
+		const getData = localStorage.getItem( 'userid' );
+		
+		if( !getData )
+			alert( '해당 키로 로컬에 저장된 데이터가 없네요~' );
+		else
+			input.value = getData;  // input 텍스트 박스에 삽입.
+		
+	} );
+	
+}
