9ccc1384924dcf8248ea98d3b58dc63f3a1fb2d7
yeonlisa <tkzw4869@gmail.com>
2022-05-03 16:43:27 +0900

    테이블의 열과 줄을 동적으로 생성하여 삽입하기(2)

--- 테이블 및 쿠키 활용/localStorage2_table_row_column.js
+++ 테이블 및 쿠키 활용/localStorage2_table_row_column.js
@@ -14,5 +14,25 @@ window.onload = () => {
         // JavaScript Table row and column.
         // Insert a row at the end of table
         const newRow0 = hTbody.insertRow();
+
+        // Insert a cell at the end of the row
+        const newCell0 = newRow0.insertCell();
+        const newCell1 = newRow0.insertCell();
+		const newCell2 = newRow0.insertCell();
+		const newCell3 = newRow0.insertCell();
+
+        // Append - 텍스트 노드를 새롭게 생성한 Cell에 붙이기
+        const newText0 = document.createTextNode('홍길동');
+        newCell0.appendChild(newText0);
+
+        const newText1 = document.createTextNode('hong@hong.com');
+        newCell1.appendChild(newText1);
+
+        const newText2 = document.createTextNode('25');
+        newCell2.appendChild(newText2);
+
+        const newText3 = document.createTextNode('영화보기');
+        newCell3.appendChild(newText3);
+
     });
 }
