6606788a898b0964d57f31d72a0d57ea767d8382
yeonlisa <tkzw4869@gmail.com>
2022-05-04 19:11:55 +0900

    자바스크립트로 쿠키(Cookie) 읽기, 생성하기, 삭제하기(3)

--- 테이블 및 쿠키 활용/cookie.js
+++ 테이블 및 쿠키 활용/cookie.js
@@ -9,7 +9,7 @@ function getCookie() {
 
     // if 조건문 -> 쿠키가 있으면
     if(allCookies != '') {
-        alert('저장된 쿠기의 값은:' + allCookies + '\n(다시 방문해주셔서 환영합니다.)');
+        alert('저장된 쿠키의 값은:' + allCookies + '\n(다시 방문해주셔서 환영합니다.)');
     } else {
         alert('저장된 쿠키가 없습니다. \n(첫 방문을 환영합니다.)');
     }
@@ -17,5 +17,20 @@ function getCookie() {
 
 // 쿠키 생성하기
 function setCookie() {
-    
+
+    // 만료일 생성
+    let expiration = new Date();
+    expiration.setDate(expiration.getDate() + 7);
+
+    // 날짜를 쿠키로 저장하기 위해서 -> UTC 방식으로 표기 -> toUTCString() 메서드 사용.
+    console.log(expiration.toUTCString());
+
+    // 쿠키 생성하기
+    let cookies = '';
+    cookies = 'userid = superman; expires = ' + expiration.toUTCString();
+
+    // 쿠키 저장하기
+    document.cookie = cookies;
+    alert('쿠키를 생성하였습니다.');
+
 }
